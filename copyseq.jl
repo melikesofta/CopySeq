0092b4f6eb912d7e02631870d64b3faa596d8052
melikesofta <msofta@ku.edu.tr>
2016-09-23 19:27:20 +0300

    get rid of stack

--- copyseq.jl
+++ copyseq.jl
@@ -8,8 +8,6 @@ an encoder-decoder machine translation model.
 module CopySeq
 
 using Knet,AutoGrad,ArgParse,Compat
-include(Pkg.dir("Knet/deprecated/src7/data/S2SData.jl"))
-include(Pkg.dir("Knet/deprecated/src7/data/SequencePerLine.jl"))
 function main(args=ARGS)
 	s = ArgParseSettings()
 	s.description="Learning to copy sequences"
@@ -26,106 +24,70 @@ function main(args=ARGS)
 	("--seed"; arg_type=Int; default=42; help="Random number seed.")
 	("--atype"; default=(gpu()>=0 ? "KnetArray{Float32}" : "Array{Float32}"); help="array type: Array for cpu, KnetArray for gpu")
 	("--fast"; action=:store_true; help="skip loss printing for faster run")
-end
-println(s.description)
-isa(args, AbstractString) && (args=split(args))
-o = parse_args(args, s; as_symbols=true)
-println("opts=",[(k,v) for (k,v) in o]...)
-o[:seed] > 0 && srand(o[:seed])
-o[:atype] = eval(parse(o[:atype]))
-global data = Any[]
-for f in o[:datafiles]
-	push!(data, S2SData(f; batchsize=o[:batchsize], ftype=Float32, dense=true, dict=o[:datafiles][1]))
-end
-vocab = maxtoken(data[1],2)
-train!(data, vocab; o=o)
+	end
+	println(s.description)
+	isa(args, AbstractString) && (args=split(args))
+	o = parse_args(args, s; as_symbols=true)
+	println("opts=",[(k,v) for (k,v) in o]...)
+	o[:seed] > 0 && srand(o[:seed])
+	o[:atype] = eval(parse(o[:atype]))
+	global data = Any[]
+	for f in o[:datafiles]
+		push!(data, process(f, o[:batchsize], o[:atype]))
+	end
+	vocab = length(data[1][1][2])
+	train!(data, vocab; o=o)
 end
 
 function train!(data, vocab; o=nothing)
 	params = initparams(vocab, o[:hidden], o[:embed], o[:winit], o[:atype])
-	s0 = initstate(o[:atype], o[:hidden], o[:batchsize])
+	state = initstate(o[:atype], o[:hidden], o[:batchsize])
 	#TODO: do dev and test
+	loss = 0
 	for epoch=1:o[:epochs]
-		@time s2s_loss = s2s_loop(params, copy(s0), data[1]; o=o)
-		o[:fast] && continue
-		println((:epoch,epoch,:loss,s2s_loss...))
+		for sentence in data[1]
+			loss += train1(params, state, sentence, vocab; o=o)
+		end
+		println((:epoch,epoch,:loss,loss))
 	end
 end
 
-function s2s_loop(params, s0, data; o=nothing)
-	#go over sentences one by one, at the end of each sentence
-	#call s2s_eos which calculates the gradloss and updates the model
-	decoding = false
-	state = s0
-	ystack = Any[]
-	losscnt = zeros(2)
-	for (x,ygold,mask) in data
-		x = convert(KnetArray{Float32}, x)
-		nwords = (mask == nothing ? size(x,2) : sum(mask))
-		if decoding && ygold == nothing # the next sentence started
-			s2s_eos(params, ystack; o=o)
-			decoding = false
-			empty!(ystack)
-		end
-		if !decoding && ygold != nothing # source ended, target sequence started
-			decoding = true
-			state[3]=copy(state[1])
-			state[4]=copy(state[2])
-		end
-		if decoding && ygold != nothing # keep decoding target
-			ygold = convert(KnetArray{Float32}, ygold)
-			s2s_decode(params, x, ygold, mask, state, decoding, ystack, nwords; losscnt=losscnt, o=o)
-		end
-		if !decoding && ygold == nothing # keep encoding source
-			s2s_encode(params, x, state, decoding; o=o)
-		end
-	end
-	s2s_eos(params, ystack; o=o)
-	return losscnt
-end
-
-function s2s_encode(params, x, state, decoding; o=nothing)
-	encdec(params, x, state, decoding; o=nothing)
-end
-
-function s2s_decode(params, x, ygold, mask, state, decoding, ystack, nwords; losscnt=nothing, o=nothing)
-	ypred = encdec(params, x, state, decoding; o=o)
-	push!(ystack, (ypred, ygold, mask))
-	if losscnt!=nothing
-		(yrows, ycols) = size(ygold)
-		losscnt[1] += loss(params, ystack; o=o)
-		losscnt[2] += nwords/ycols
-	end
-	return ystack
-end
-
-function s2s_eos(params, ystack; o=nothing)
-	println(loss(params, ystack; o=o))
-	gloss = lossgradient(params, ystack; o=o)
-	println(typeof(gloss))
-	#println(size(gloss))
-	for k in 1:length(params)
-		params[k] -= o[:lr] * gloss[k]
+function train1(params, state, sentence, vocab; o=nothing)
+	gloss = lossgradient(params, sentence, state, vocab; o=o)
+	for i=1:length(params)
+		params[i] -= o[:lr] * gloss[i]
 	end
 	for i = 1:length(state)
 		isa(state,Value) && error("State should not be a Value.")
 		state[i] = getval(state[i])
 	end
+	return loss(params, sentence, state, vocab; o=o)
 end
 
-function loss(params, ystack; o=nothing)
-	total = 0.0; count = 0
-	atype = typeof(getval(params[1]))
-	println(size(ystack))
-	for i=1:length(ystack)-1
-		(ypred, ygold, mask) = ystack[i]
-		if mask != 0 && ygold!=nothing && ypred!=nothing
-			ynorm = logp(ypred,2) # ypred .- log(sum(exp(ypred),2))
-			total += sum(ygold .* ynorm)
-			count += size(ygold,1)
-		end
+function loss(params, sentence, state, vocab; o=nothing) #divide loss by num of words
+	loss = 0
+	decoding=false
+	for word in sentence
+		encdec(params, word, state, decoding; o=o)
 	end
-	return -total / count
+	state[3]=state[1]
+	state[4]=state[2]
+	decoding=true
+	ypred = zeros(o[:batchsize], vocab)
+	ypred[vocab] = 1.0
+	ypred = convert(o[:atype], ypred)
+	for word in sentence
+		ygold = word
+		ypred = encdec(params, ypred, state, decoding; o=o)
+		total = 0.0; count = 0
+		atype = typeof(getval(params[1]))
+		ynorm = logp(ypred,2) # ypred .- log(sum(exp(ypred),2))
+		total += sum(ygold .* ynorm)
+		count += size(ygold,1)
+		loss += -total / count
+	end
+	nwords = length(sentence)
+	return loss / nwords
 end
 
 lossgradient = grad(loss)
@@ -157,13 +119,11 @@ function initstate(atype, hidden, batchsize)
 end
 
 function encdec(params, x, state, decoding; o=nothing)
-	# here I take the transpose of x in order to get a row vector input
-	# since S2SData returns the data in column vectors
 	if !decoding
-		emb = x' * params[5]
+		emb = x * params[5]
 		h = lstm(params, state, 1, emb)
 	else
-		emb = x' * params[6]
+		emb = x * params[6]
 		h = lstm(params, state, 3, emb)
 		ypred = h[3] * params[7] .+ params[8]
 		return exp(logp(ypred))
@@ -183,6 +143,36 @@ function lstm(param,state,index,input; o=nothing)
 	hidden  = outgate .* tanh(cell)
 	(state[index],state[index+1]) = (hidden,cell)
 	return state
+end
+
+function process(datafile, batchsize, atype)
+	#TODO: optimize memory usage
+	dict = readvocab(datafile)
+	data, line = Any[], Any[]
+	word = nothing
+	open(datafile) do f
+		for l in eachline(f)
+				for w in split(l)
+					word = zeros(Float64, batchsize, length(dict))
+					word[dict[w]] = 1.0
+					push!(line, copy(convert(atype, word)))
+				end
+				push!(data, copy(line))
+		end
+	end
+	return data
+end
+
+function readvocab(file) # TODO: test with cmd e.g. `zcat foo.gz`
+    d = Dict{Any,Int}()
+    open(file) do f
+        for l in eachline(f)
+            for w in split(l)
+                get!(d, w, 1+length(d))
+            end
+        end
+    end
+    return d
 end
 
 end #module
