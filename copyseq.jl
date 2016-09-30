8ef918aaf54458724718dd56efcd9910ae6d5c15
melikesofta <msofta@ku.edu.tr>
2016-09-30 16:43:05 +0300

    add gclip

--- copyseq.jl
+++ copyseq.jl
@@ -13,56 +13,64 @@ function main(args=ARGS)
 	s.description="Learning to copy sequences"
 	s.exc_handler=ArgParse.debug_handler
 	@add_arg_table s begin
-		("--datafiles"; nargs='+'; help="If provided, use first file for training, second for dev, others for test.")
-		("--hidden"; arg_type=Int; default=256; help="Sizes of one or more LSTM layers.")
-		("--embed"; arg_type=Int; default=256; help="Size of the embedding vector.")
-		("--epochs"; arg_type=Int; default=3; help="Number of epochs for training.")
-		("--batchsize"; arg_type=Int; default=1; help="Number of sequences to train on in parallel.")
-		("--lr"; arg_type=Float64; default=1.0; help="Initial learning rate.")
-		("--gclip"; arg_type=Float64; default=1.0; help="Value to clip the gradient norm at.")
-		("--winit"; arg_type=Float64; default=0.01; help="Initial weights set to winit*randn().")
-		("--seed"; arg_type=Int; default=42; help="Random number seed.")
-		("--atype"; default=(gpu()>=0 ? "KnetArray{Float32}" : "Array{Float32}"); help="array type: Array for cpu, KnetArray for gpu")
-		("--fast"; action=:store_true; help="skip loss printing for faster run")
-	end
-	println(s.description)
-	isa(args, AbstractString) && (args=split(args))
-	o = parse_args(args, s; as_symbols=true)
-	println("opts=",[(k,v) for (k,v) in o]...)
-	o[:seed] > 0 && srand(o[:seed])
-	o[:atype] = eval(parse(o[:atype]))
-	global data = Any[]
-	for f in o[:datafiles]
-		push!(data, process(f, o[:batchsize], o[:atype]))
-	end
-	vocab = length(data[1][1][2])
-	train!(data, vocab; o=o)
+	("--datafiles"; nargs='+'; help="If provided, use first file for training, second for dev, others for test.")
+	("--hidden"; arg_type=Int; default=256; help="Sizes of one or more LSTM layers.")
+	("--embed"; arg_type=Int; default=256; help="Size of the embedding vector.")
+	("--epochs"; arg_type=Int; default=3; help="Number of epochs for training.")
+	("--batchsize"; arg_type=Int; default=1; help="Number of sequences to train on in parallel.")
+	("--lr"; arg_type=Float64; default=1.0; help="Initial learning rate.")
+  ("--gclip"; arg_type=Float64; default=3.0; help="Value to clip the gradient norm at.")
+	("--winit"; arg_type=Float64; default=0.01; help="Initial weights set to winit*randn().")
+	("--seed"; arg_type=Int; default=42; help="Random number seed.")
+	("--atype"; default=(gpu()>=0 ? "KnetArray{Float32}" : "Array{Float32}"); help="array type: Array for cpu, KnetArray for gpu")
+	("--fast"; action=:store_true; help="skip loss printing for faster run")
+end
+println(s.description)
+isa(args, AbstractString) && (args=split(args))
+o = parse_args(args, s; as_symbols=true)
+println("opts=",[(k,v) for (k,v) in o]...)
+o[:seed] > 0 && srand(o[:seed])
+o[:atype] = eval(parse(o[:atype]))
+global data = Any[]
+for f in o[:datafiles]
+	push!(data, process(f, o[:batchsize], o[:atype]))
+end
+vocab = length(data[1][1][2])
+train!(data, vocab; o=o)
 end
 
 function train!(data, vocab; o=nothing)
 	params = initparams(vocab, o[:hidden], o[:embed], o[:winit], o[:atype])
 	state = initstate(o[:atype], o[:hidden], o[:batchsize])
 	#TODO: do dev and test
-	loss = 0
 	for epoch=1:o[:epochs]
+		loss = 0
+		sent_cnt = 0
 		for sentence in data[1]
 			loss += train1(params, state, sentence, vocab; o=o)
+			sent_cnt += 1
 		end
-		println((:epoch,epoch,:loss,loss))
+		println((:epoch,epoch,:loss,loss/sent_cnt))
 	end
 end
 
 function train1(params, state, sentence, vocab; o=nothing)
-	#println(loss(params, sentence, state, vocab; o=o))
 	gloss = lossgradient(params, sentence, state, vocab; o=o)
-	for i=1:length(params)
-		#println("gloss ", i, ": ", gloss[i][1])
-		params[i] -= o[:lr] * gloss[i]
+	gscale = o[:lr]
+	gclip = o[:gclip]
+	if gclip > 0
+		gnorm = sqrt(mapreduce(sumabs2, +, 0, gloss))
+		if gnorm > gclip
+			gscale *= gclip / gnorm
+		end
 	end
-	#for i = 1:length(state)
-	#	isa(state,Value) && error("State should not be a Value.")
-	#	state[i] = getval(state[i])
-	#end
+	for k in 1:length(params)
+		params[k] -= gscale * gloss[k]
+	end
+	for i = 1:length(state)
+		isa(state,Value) && error("State should not be a Value.")
+		state[i] = getval(state[i])
+	end
 	return loss(params, sentence, state, vocab; o=o)
 end
 
@@ -80,7 +88,7 @@ function loss(params, sentence, state, vocab; o=nothing)
 
 	# give <eos> as the first token into decoder
 	ypred = zeros(o[:batchsize], vocab)
-	ypred[vocab] = 1.0
+	ypred[vocab] = 1
 	ypred = convert(o[:atype], ypred)
 
 	total = 0.0; count = 0 # for loss calculations
@@ -89,11 +97,13 @@ function loss(params, sentence, state, vocab; o=nothing)
 	for ygold in sentence
 		state, ypred = encdec(params, input, state, decoding; o=o) #predict
 		ynorm = logp(ypred,2) # ypred .- log(sum(exp(ypred),2))
+		#	println(sum(ygold .* ynorm))
 		total += sum(ygold .* ynorm)
 		count += size(ygold,1)
 		input = ygold
 	end
-	return -total/count
+	#println("loss for sentence: ", total/count)
+	return total/count
 end
 
 lossgradient = grad(loss)
@@ -158,28 +168,28 @@ function process(datafile, batchsize, atype)
 	word = nothing
 	open(datafile) do f
 		for l in eachline(f)
-				for w in split(l)
-					word = zeros(Float64, batchsize, length(dict))
-					word[dict[w]] = 1.0
-					push!(line, copy(convert(atype, word)))
-				end
-				push!(data, copy(line))
-				empty!(line)
+			for w in split(l)
+				word = zeros(Float64, batchsize, length(dict))
+				word[dict[w]] = 1.0
+				push!(line, copy(convert(atype, word)))
+			end
+			push!(data, copy(line))
+			empty!(line)
 		end
 	end
 	return data
 end
 
 function readvocab(file)
-    d = Dict{Any,Int}()
-    open(file) do f
-        for l in eachline(f)
-            for w in split(l)
-                get!(d, w, 1+length(d))
-            end
-        end
-    end
-    return d
+	d = Dict{Any,Int}()
+	open(file) do f
+		for l in eachline(f)
+			for w in split(l)
+				get!(d, w, 1+length(d))
+			end
+		end
+	end
+	return d
 end
 
 end #module
