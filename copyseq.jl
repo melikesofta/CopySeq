13a4dfbcb30d2cc30b1061b34d79248687586077
melikesofta <msofta@ku.edu.tr>
2016-10-14 01:19:11 +0300

    correct indentation

--- copyseq.jl
+++ copyseq.jl
@@ -19,9 +19,9 @@ function main(args=ARGS)
 	("--epochs"; arg_type=Int; default=3; help="Number of epochs for training.")
 	("--batchsize"; arg_type=Int; default=1; help="Number of sequences to train on in parallel.")
 	("--lr"; arg_type=Float64; default=1.0; help="Initial learning rate.")
-  ("--gclip"; arg_type=Float64; default=3.0; help="Value to clip the gradient norm at.")
+	("--gclip"; arg_type=Float64; default=3.0; help="Value to clip the gradient norm at.")
 	("--winit"; arg_type=Float64; default=0.01; help="Initial weights set to winit*randn().")
- 	("--gcheck"; arg_type=Int; default=0; help="Check N random gradients.")
+	("--gcheck"; arg_type=Int; default=0; help="Check N random gradients.")
 	("--seed"; arg_type=Int; default=42; help="Random number seed.")
 	("--atype"; default=(gpu()>=0 ? "KnetArray{Float32}" : "Array{Float32}"); help="array type: Array for cpu, KnetArray for gpu")
 	("--fast"; action=:store_true; help="skip loss printing for faster run")
@@ -52,8 +52,8 @@ function train!(data, vocab; o=nothing)
 			lss += train1(params, state, sentence, vocab; first_loss=first_loss, o=o)
 			sent_cnt += 1
 			if o[:gcheck] > 0 && sent_cnt == 1 #check gradients only for one sentence
-			 	gradcheck(loss, params, sentence, copy(state), vocab; gcheck=o[:gcheck], o=o)
-		 	end
+				gradcheck(loss, params, sentence, copy(state), vocab; gcheck=o[:gcheck], o=o)
+			end
 		end
 		first_loss=false
 		println((:epoch,epoch,:loss,lss/sent_cnt))
